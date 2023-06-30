0dd0dcce092446c4cfb45f35de11055b28d8da43
Felixxx_Note <f3lixxx@yandex.ru>
2023-06-30 16:26:19 +0300

    HW3_done

--- HW03/main.cpp
+++ HW03/main.cpp
@@ -2,8 +2,8 @@
 
 class calculator {
 public:
-    int num1;
-    int num2;
+    double num1;
+    double num2;
 
     void input() {
         std::cout << "Введите num1: ";
@@ -42,50 +42,59 @@ public:
 
     double divide_1_2() {
         int div1;
-        div1 = num1 - num2;
-        std::cout << "num1" << '-' << "num2" << " = " << div1 << std::endl;
+        div1 = num1 / num2;
+        std::cout << "num1" << '/' << "num2" << " = " << div1 << std::endl;
         return div1;
+    }
+
+      bool set_num1(double num1) {
+          if (this->num1 == 0){
+              std::cout << "Неверный ввод! " << std::endl;
+              std::cout << "Введите num1: ";
+              std::cin >> this-> num1;
+              return false;
+      }
+          else {
+              return true;
+          }
+      }
+
+    bool set_num2(double num2) {
+        if (this->num2 == 0) {
+            std::cout << "Неверный ввод! " << std::endl;
+            std::cout << "Введите num2: ";
+            std::cin >> this-> num1;
+            return false;
+        }
+
+        else {
+            return true;
+        }
     }
 
     double divide_2_1() {
         int div2;
-        div2 = num1 - num2;
-        std::cout << "num1" << '-' << "num2" << " = " << div2 << std::endl;
+        div2 = num1 / num2;
+        std::cout << "num1" << '/' << "num2" << " = " << div2 << std::endl;
         return div2;
-    }
-
-    bool set_num1() {
-        if (num1 == 0){
-            std::cout << "Неверный ввод! " << std::endl;
-        std::cout << "Введите num1: ";
-        std::cin >> num1;
-    }
-        return num1;
-    }
-
-    bool set_num2() {
-        if (num2 == 0) {
-            std::cout << "Неверный ввод! " << std::endl;
-            std::cout << "Введите num2: ";
-            std::cin >> num2;
-        }
-        return num2;
     }
     };
 
     int main() {
+        double number1;
+        double number2;
             calculator res{};
             while (true) {
                 res.input();
-                res.set_num1();
-                res.set_num2();
+                res.set_num1(number1);
+                res.set_num2(number2);
                 res.add();
                 res.multiply();
                 res.subtract_1_2();
                 res.subtract_2_1();
                 res.divide_1_2();
                 res.divide_2_1();
-
+                std::cout << '\n';
             }
       return 0;
     }
