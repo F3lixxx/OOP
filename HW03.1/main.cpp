5f95d7db64c42760dff58a050f6e5d87dd41a41c
yenox <felixxx@vivaldi.net>
2023-07-05 13:12:00 +0300

    done! HW03.1

--- HW03.1/main.cpp
+++ HW03.1/main.cpp
@@ -4,33 +4,59 @@
 class counter{
     private :
     int incr=1;
-    std::string point;
 public:
-    std::string add(){
-            std::string point;
-            std:: cout << "Введите команду ('+', '-', '=' или 'x'): ";
-            std::cin >> point;
-            if (point == "+") {
-                incr += 1;
-            } else if (point == "-") {
-                incr--;
-            } else if (point == "=") {
-                std::cout << incr << std::endl;
-            }
-            if (point == "x") {
+    void set_incr(){
+        std:: string a;
+        std:: cout << "Do you want to specify an initial counter value? Enter yes or no: ";
+        std:: cin >> a;
+        if (a == "yes") {
+            std:: cout << "Enter the initial counter value: ";
+            std:: cin >> incr;
+        }
+    }
+    std::string ex(){
+        std::string exit;
+        while(true) {
+            if (exit == "x") {
                 std::cout << "До свидания!" << std::endl;
-                return 0;
+                break;
             }
         }
-};
+    }
+    bool add(std:: string& value) {
+
+            if (value == "+") {
+                incr += 1;
+                return false;
+            }
+            else if (value == "-") {
+                incr--;
+                return false;
+            }
+            else if (value == "=") {
+                std::cout << incr << std::endl;
+                return false;
+            }
+            if(value == "x"){
+                std:: cout << "До свидания!";
+                return true;
+            }
+        }
+    };
 
 int main() {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
 
     counter s;
-    while(true) {
-        s.add();
+    std:: string value;
+    s.set_incr();
+   while(true) {
+       std::cout << "Enter the command ('+', '-', '=' or 'x'): ";
+       std::cin >> value;
+        s.add(value);
+        if (value == "x"){
+            break;
+        }
     }
-    return 0;
 }
