41600bae06a5d71cefe60df79552d83e79dd3753
yenox <felixxx@vivaldi.net>
2023-07-08 14:04:17 +0300

    change code HW03.1

--- HW03.1/main.cpp
+++ HW03.1/main.cpp
@@ -5,7 +5,7 @@ class counter{
     private :
     int incr=1;
 public:
-    void set_incr(){
+    counter (){
         std:: string a;
         std:: cout << "Do you want to specify an initial counter value? Enter yes or no: ";
         std:: cin >> a;
@@ -14,15 +14,7 @@ public:
             std:: cin >> incr;
         }
     }
-    std::string ex(){
-        std::string exit;
-        while(true) {
-            if (exit == "x") {
-                std::cout << "До свидания!" << std::endl;
-                break;
-            }
-        }
-    }
+
     bool add(std:: string& value) {
             if (value == "+") {
                 incr += 1;
@@ -37,7 +29,7 @@ public:
                 return false;
             }
             if(value == "x"){
-                std:: cout << "До свидания!";
+                std:: cout << "BYE!";
                 return true;
             }
         }
@@ -49,7 +41,6 @@ int main() {
 
     counter s;
     std:: string value;
-    s.set_incr();
    while(true) {
        std::cout << "Enter the command ('+', '-', '=' or 'x'): ";
        std::cin >> value;
