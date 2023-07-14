087e4720957b0320b23828ac7b1a0faa95b18b23
Felixxx_Note <f3lixxx@yandex.ru>
2023-07-14 14:01:43 +0300

    edit in noutbook

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <windows.h>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -7,34 +6,37 @@
 
 
 int main() {
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-
-
-
     std::string sizeM;
     std::string sizeN;
 
-    std::ifstream fin("D:\\ClionProjects\\OOP\\HW4\\in.txt"); //винда
-    std::ofstream fout("D:\\ClionProjects\\OOP\\HW4\\out.txt"); //винда
 
+   // std::ifstream fin("D:\\ClionProjects\\OOP\\HW4\\in.txt"); //винда
+   // std::ofstream fout("D:\\ClionProjects\\OOP\\HW4\\out.txt"); //винда
+    std::ifstream fin("in.txt");
+    std::ofstream fout("out.txt");
     if(fin.is_open()) {
         while(!fin.eof()){
-            for (int j = 0; j < 1; ++j) {
+            for (int j = 0; j < 1; j++) {
                 fin >> sizeM;
-                fout << sizeM;
+                fout << sizeM << "\n";
                 std::cout << sizeM << '\n';
             }
             while(!fin.eof()) {
-                for (int i = 4; i > 0; i--) {
+                for (int i = 0; i < 4; i++) {
                     fin >> sizeN;
-                    fout << sizeN;
+                    fout << sizeN << ',' << ' ';
                     std::cout << sizeN << ',' << ' ';
                 }
+                std::cout << sizeN[4];
+                fout <<"\n";
                 std::cout << '\n';
             }
             fin.close();
         }
     }
-        return 0;
+    else {
+        std::cout << "File is not open";
     }
+
+    return 0;
+}
