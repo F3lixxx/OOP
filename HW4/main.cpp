49ec69585b0567037a33af221652f546d3002e38
Felixxx_Note <f3lixxx@yandex.ru>
2023-07-15 13:48:11 +0300

    change code in HW04

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <windows.h>
+//#include <windows.h>
 
-
-
-int main() {
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-
+class adress {
+private:
     std::string sizeM;
-    std::string sizeN = "";
-
-
-   // std::ifstream fin("D:\\ClionProjects\\OOP\\HW4\\in.txt"); //винда
-   // std::ofstream fout("D:\\ClionProjects\\OOP\\HW4\\out.txt"); //винда
-    std::ifstream fin("in.txt");
-    std::ofstream fout("out.txt");
-    if(fin.is_open()) {
-        while(!fin.eof()){
-            for (int j = 0; j < 1; j++) {
-                fin >> sizeM;
-                fout << sizeM << "\n";
-                std::cout << sizeM << '\n';
-            }
-            while(!fin.eof()) {
-                for (int i = 0; i < 4; i++) {
-                    fin >> sizeN;
-                    if ( i != 0 ) {
-                        std::cout << ',' << ' ';
-                        fout << ',' << ' ';
-                    }
-                    fout << sizeN;
-                    std::cout << sizeN;
+    std::string sizeN;
+public:
+    void set_words() {
+        std::ifstream fin("in.txt");
+        std::ofstream fout("out.txt");
+        int size = 0;
+        fin >> size;
+        std::string *dynamic = new std::string[size];
+        if (fin.is_open()) {
+            while (!fin.eof()) {
+                for (int j = 0; j < size; j++) {
+                    fin >> dynamic[j];
+                    fout << dynamic[j] << "\n";
+                    std::cout << dynamic[j] << '\n';
                 }
-                fout <<"\n";
+                /*int mass;
+                fin >> mass;
+                std::string* dynamic2 = new std::string [mass];
+                while (!fin.eof()) {
+                    for (int i = 0; i < mass; i++) {
+                        fin >> mass[i];
+                        if (i != 0) {
+                            std::cout << ',' << ' ';
+                            fout << ',' << ' ';
+                        }
+                        fout << mass[i];
+                        std::cout << mass[i];
+                    }*/
+/*
+                fout << "\n";
                 std::cout << '\n';
+*/
             }
             fin.close();
         }
     }
-    else {
-        std::cout << "File is not open";
-    }
+ /*       else {
+            std::cout << "File is not open";
+        }
+    }*/
+};
+
+
+int main() {
+    //SetConsoleCP(CP_UTF8);
+    //SetConsoleOutputCP(CP_UTF8);
+
+    adress house;
+    house.set_words();
 
     return 0;
 }
