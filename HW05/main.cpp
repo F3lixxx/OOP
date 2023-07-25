079451c1faf6d042bf4237df18fa4b9691968d77
Felixxx_Note <f3lixxx@yandex.ru>
2023-07-25 20:31:29 +0300

    edit HW05

--- HW05/main.cpp
+++ HW05/main.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
 
-class figure {
+class Figure {
 protected:
-    int sides_count = 0;
-    std::string name = "Фигура: ";
+    int sides_a = 0;
+    int sides_b = 0;
+    int sides_c = 0;
+    int sides_d = 0;
+    int angle_A = 0;
+    int angle_B = 0;
+    int angle_C = 0;
+    int angle_D = 0;
+    std::string name_figure;
+    bool four_angle = true;
 
 public:
-    figure(int side_count) : sides_count(side_count) {}
+    std::string get_name_figure(){ return name_figure;}
 
-    void get_sides_count() {
-        std::cout << name << sides_count << std::endl;
-    }
+    int get_sides_a(){ return sides_a;}
+    int get_sides_b(){ return sides_b;}
+    int get_sides_c(){ return sides_c;}
+    int get_sides_d(){ return sides_d;}
+    int get_angle_A(){ return angle_A;}
+    int get_angle_B(){ return angle_B;}
+    int get_angle_C(){ return angle_C;}
+    int get_angle_D(){ return angle_D;}
+
 };
 
-class triangle : public figure {
+class triangle : public Figure {
 public:
-    triangle(int side_trian) : figure(side_trian) {
-        std::string name2 = "Трехугольник: ";
-        std::cout << name2 << side_trian << std::endl;
+    triangle(int side_a, int side_b, int side_c, int side_A, int side_B, int side_C) {
+         std::string name_figure = "Треугольник: \n";
+        side_a = sides_a;
+        side_b = sides_b;
+        side_c = sides_c;
+        side_A = angle_A;
+        side_B = angle_B;
+        side_C = angle_C;
     }
-};
 
-class quadrangle : public figure {
-public:
-    quadrangle(int side_quadr) : figure(side_quadr) {
-        std::string name1 = "Четырехугольник: ";
-        std::cout << name1 << side_quadr << std::endl;
-    }
 };
+void print_fig(Figure &triangle){
+    std::cout << '\n';
+    std::cout << triangle.get_name_figure() << ":" << std::endl;
+
+    std::cout << "Стороны:";
+    std:: cout << "a=" << triangle.get_sides_a() <<", b=" << triangle.get_sides_b() << ", c=" << triangle.get_sides_c() << std::endl;
+}
 
 
 int main() {
     std::cout << "Количество сторон:" << std::endl;
-    figure f(0);
-    f.get_sides_count();
-    triangle t(3);
-    quadrangle q(4);
+    triangle triangl(1, 2, 3, 4, 5, 6);
+    print_fig(triangl);
 
+    return 0;
 }
