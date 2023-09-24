7a9419ecb64829ba3e7cf8dcd74a46d1a3aa9cca
yenox <felixxx@vivaldi.net>
2023-09-24 18:42:01 +0300

    add HW10

---
+++ HW09_Operator overload/HW01/build/CMakeFiles/HW01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW01.dir/main.cpp.obj"
+  "CMakeFiles/HW01.dir/main.cpp.obj.d"
+  "HW01.exe"
+  "HW01.exe.manifest"
+  "HW01.pdb"
+  "libHW01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- HW09_Operator overload/HW01/main.cpp
+++ HW09_Operator overload/HW01/main.cpp
@@ -13,9 +13,9 @@ public:
         numerator_ = numerator;
         denominator_ = denominator;
     }
-    double frac() { return std::sqrt(numerator_ * numerator_ + denominator_ * denominator_); }
-    bool operator==(const Fraction& other) const = default;
-    bool operator>(Fraction other) { return frac() < other.frac(); }
+    double firstfrac(Fraction& first) { return (numerator_ * first.denominator_ && first.numerator_ * denominator_); }
+   // bool operator==(const Fraction& other) const = default;
+    bool operator>(Fraction other) { return firstfrac(other) < other.firstfrac(other); }
     bool operator<(Fraction other) { return other > *this; }
     bool operator>=(Fraction other) { return !(*this < other); }
     bool operator<=(Fraction other) { return !(*this > other); }
@@ -23,14 +23,15 @@ public:
 
 int main()
 {
-    Fraction f1(4, 3);
-    Fraction f2(6, 11);
+    Fraction f1(1, 1);
+    Fraction f2(4, 2);
 
-    std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
-    std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+  //  std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+  //  std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
     std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
     std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
     std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
     std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+    system("pause");
     return 0;
 }
