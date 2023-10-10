edd87d9ab23621a55467eee24034411862563a52
Yenox.tecno <f3lixxx@yandex.ru>
2023-10-10 19:21:47 +0300

    change code in kurs

--- kurs/main.cpp
+++ kurs/main.cpp
@@ -6,6 +6,7 @@ int choose = 0;
 class type_race{
 protected:
     std::string name_race_;
+    std::string stop = "0 for Exit";
 
     std::string camel;
     std::string fast_camel;
@@ -26,6 +27,8 @@ public:
     std::string getflying_carpet(){ return flying_carpet; }
     std::string geteagle(){ return eagle; }
     std::string getbroom(){ return broom; }
+
+    std::string getstop(){ return stop; };
 
 };
 
@@ -64,76 +67,95 @@ void print_information(type_race &race) {
             std::cout << " Length: " << length << std::endl;
 }
 
-void print(type_race &race){
-    if (choose == 1){
-        std::cout << "1 "<< race.getcamel() << "\n2 "<< race.getfast_camel()
-                  << "\n3 "<< race.getcentaur() << "\n4 " << race.getbicycle_shoes() << std::endl;
+void print(type_race &race, std::set<std::string> &trans){
+    int size;
+    if (choose == 1) {
+        std::cout << "1 " << race.getcamel() << "\n2 " << race.getfast_camel()
+                  << "\n3 " << race.getcentaur() << "\n4 " << race.getbicycle_shoes() << std::endl << race.getstop() << std::endl;
+        while(true) {
+            std::cout << "Minimum of 2 vehicles must be registered: ";
+            std::cin >> size;
+            if(size == 0){
+                break;
+            }
+            std::string tr;
+            if (size == 1) {
+                tr = race.getcamel();
+            } else if (size == 2) {
+                tr = race.getfast_camel();
+            } else if (size == 3) {
+                tr = race.getcentaur();
+            } else if (size == 4) {
+                tr = race.getbicycle_shoes();
+            } else {
+                std::cout << "Error choose from 1 to 4.\n";
+                continue;
+            }
+
+            if (trans.count(tr) > 0) {
+                std::cout << "this transport is registered yet.\n";
+            } else {
+                trans.insert(tr);
+                std::cout << "transport " << tr << " is added.\n";
+            }
+        }
     }
     else{
         std::cout << "1 "<< race.getflying_carpet() << "\n2 "<< race.getbroom()
-                  << "\n3 "<< race.geteagle() << std::endl;
+                  << "\n3 "<< race.geteagle() << race.getstop() << std::endl;
+        while(true) {
+            std::cout << "Minimum of 2 vehicles must be registered: ";
+            std::cin >> size;
+            if(size == 0){
+                break;
+            }
+            std::string tr;
+            if (size == 1) {
+                tr = race.getflying_carpet();
+            } else if (size == 2) {
+                tr = race.getbroom();
+            } else if (size == 3) {
+                tr = race.geteagle();
+            }  else {
+                std::cout << "Error choose from 1 to 3.\n";
+                continue;
+            }
+
+            if (trans.count(tr) > 0) {
+                std::cout << "this transport is registered yet.\n";
+            } else {
+                trans.insert(tr);
+                std::cout << "transport " << tr << " is added.\n";
+            }
+        }
     }
 }
-
-void choose_transport(type_race &chose){
-   int size;
-
-    while (true) {
-        std::cout << "Minimum of 2 vehicles must be registered";
-        std::cin >> size;
-        if (size == 1) {
-            std::cout << chose.getcamel();
-        } else if (size == 2) {
-            std::cout << chose.getfast_camel();
-        } else if (size == 3) {
-            std::cout << chose.getcentaur();
-        } else {
-            std::cout << chose.getbicycle_shoes();
-        }
-        if (size == 1 || size == 2 || size == 3 || size == 4) {
-            throw std::runtime_error("This transport is registered yet");
-
-        }
-    }
-}
-
-
 
 int main() {
-    int chooose;
-
     ground ground_1;
     air air_1;
+    std::set<std::string> n;
     std::cout << "Welcome to Racing simulator \n"
                  "1. Racing for land transport\n"
                  "2. Racing for fly transport\n";
 
     while ((choose < 1) || (choose > 2)) {
-        std::cout << "Choose type of racing ";
+        std::cout << "Choose type of racing";
         std::cin >> choose;
     if (choose == 1) {
-        ground_1;
         print_information(ground_1);
-        print(ground_1);
-
+        print(ground_1, n);
     }
     else if ( choose == 2){
-        air_1;
-        print_information(air_1);
-        print(air_1);
 
+        print_information(air_1);
+        print(air_1, n);
     }
     else{
         if ((choose < 1) || (choose > 2)) {
             std::cout << "Wrong type!\n";
         }
     }
-    }
-    try{
-        choose_transport(ground_1);
-    }
-    catch (const std::runtime_error& e) {
-        std::cerr  << e.what() << std::endl;
     }
     return 0;
 }
